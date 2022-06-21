423aaefd58986ef644da6473e4ca5291073405dd
Erhan <erhantezer68@gmail.com>
2022-06-21 23:11:06 +0300

    new folder was added

---
+++ 1-RC/my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    //? JSX
+    <div className="App">
+      <h1> merhaba react</h1>
+    </div>
+  );
+}
+
+export default App;
