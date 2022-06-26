661aee4e4796ee4cf9a3aa2c4001a38665c0543d
Erhan <erhantezer68@gmail.com>
2022-06-26 03:53:24 +0300

    new project was added

---
+++ React-projects/Tours/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+
+  
+  //? useState kullanımı tours  başlangıç boş = [] setTours değiştirecek sonra
+  const [tours, setTours] = useState([])
+
+  // ? not interested butonuuna bastığımda tour.id boş olduğunda id eşitliği sağlamayacak ve 
+  //? [] newTours a atanacak böylelikle api boş olacak refresh yaptığımızda fetchTours çalışacak api tours içine atanacak ve tekrar çalışacak
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+
+
+  //? api çekme işlemi ve tours değeri olarak atanması önemli yukarda kullanmak için
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+
+      //? setTours değeri tekrar dolduruldu
+      setTours(tours)
+    } catch (error) {
+      
+      console.log(error)
+    }
+  }
+
+
+  //? başlangıç olarak çağırmak için kullandık
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+ 
+  //? refresh yaparak fetchTours çağırıp tekrar api çektik ve görüntüler geldi
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    //? ana klasör tamamı main içinde önce Tours.js gönderdik 
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
+}
+
+export default App
---
+++ React-projects/Tours/src/Tours.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Tour from './Tour';
+
+//? tours ve remove propslarını aldık
+const Tours = ({ tours, removeTour }) => {
+  return (
+    //? Tours main içinde section oluşturdu
+    <section>
+    <div className='title'>
+      <h1>Ours Tours</h1>
+      <div className='underline'></div>
+      <div>
+        //? map le tours un içine girerek parçalayıp Tour dosyasına gönderdik
+        {tours.map((tour)=>{
+          
+          console.log(tour);
+          return(
+            <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+          )
+        })}
+      </div>
+    </div>
+    </section>
+  )
+};
+
+export default Tours;
