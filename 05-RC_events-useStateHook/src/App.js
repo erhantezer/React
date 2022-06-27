18b41a2954a48b32c9e2caa08e21d06b39e8f30d
Erhan <erhantezer68@gmail.com>
2022-06-28 00:27:03 +0300

    updated

--- 05-RC_events-useStateHook/src/App.js
+++ 05-RC_events-useStateHook/src/App.js
@@ -2,12 +2,16 @@ import Counter from "./2-classComponent/Counter";
 import Events from "./1-event-intro/Events";
 import UseStateExample from "./components/3-hooksState/UseStateExample";
 
+
 function App() {
   return (
     <div>
       <Events/>
+
       <Counter count={10}/>
+
       <Counter />
+      
       <UseStateExample/>
     </div>
   );
