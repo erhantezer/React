11bf201688d45464d4a30745d2d5309542673efa
Erhan <erhantezer68@gmail.com>
2022-06-28 22:01:02 +0300

    keyboard

--- 06-RC_forms_otherEvents/src/App.js
+++ 06-RC_forms_otherEvents/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      {/* <KeyboardEvents />
-      <MouseEvent /> */}
-      <FormObject/>
+      <KeyboardEvents />
+      {/* <MouseEvent /> */} 
+      {/* <FormObject/> */}
     </div>
   );
 }
--- 06-RC_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
+++ 06-RC_forms_otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
@@ -1,8 +1,34 @@
+import { useState } from "react"
 
 
 const KeyboardClipboard = () => {
+
+  const [inputValue, setInputValue] = useState("");
+
+  // const handleInputChange = () => {
+
+  // }
+
+
+const handleKeyDown = (e) => {
+// console.log(e.keyCode);
+e.target.value = e.target.value.toUpperCase()
+// e.keyCode === 13 && alert("Entera basıldı")
+}
+
+
   return (
-    <div>KeyboardClipboard</div>
+    <div className="container text-center">
+      <h2>CLIBOARD EVENTS</h2>
+      <input type="text" value ={inputValue} 
+      onChange={(e) => setInputValue(e.target.value)}
+      // onChange={(e) => setInputValue(e.target.value.toUpperCase())}
+      onKeyDown={handleKeyDown}
+      />
+
+      <p className="text-start m-4">{inputValue}</p>
+
+    </div>
   )
 }
 
