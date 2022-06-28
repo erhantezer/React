35ba777324c48ad210c165a3c795f385ecd52de0
Erhan <erhantezer68@gmail.com>
2022-06-28 20:56:34 +0300

    form

--- 06-RC_forms_otherEvents/src/components/1-forms/Form.jsx
+++ 06-RC_forms_otherEvents/src/components/1-forms/Form.jsx
@@ -1,9 +1,58 @@
-
+import { useState } from "react";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
+
+const [username, setUsername] = useState("")
+const [email,setEmail] = useState("")
+const [password,setPassword] = useState("")
+
+
+const handleUsername = (e) =>{
+  // console.log(e.target);
+  setUsername(e.target.value)
 }
 
-export default Form
+const handleSubmit = (e) => {
+  // e.preventDefault();
+  alert(
+  ` username: ${username}
+    email: ${email}
+    password: ${password}
+    `
+  )
+  setEmail("");
+  setPassword("");
+  setPassword("");
+  // e.target.reset();
+}
+  return (
+    <div>
+      <form style={{ margin: '5rem' }} className="d-flex flex-column" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">Username: <span className="text-warning">{username}</span>
+           
+          </label>
+          <input type="text" className="form-control" id="username" onChange={handleUsername} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address <span className="text-danger">{email}</span>
+          </label>
+          <input type="email" className="form-control" id="email" onChange={(e)=> setEmail(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password: <span className="text-info">{password} </span>
+          </label>
+          <input type="password" className="form-control" id="password" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
