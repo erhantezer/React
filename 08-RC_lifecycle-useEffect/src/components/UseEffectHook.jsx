35981d903e774141d37939ca0287f3a7795a2ba7
Erhan <erhantezer68@gmail.com>
2022-07-01 00:23:32 +0300

    updated

--- 08-RC_lifecycle-useEffect/src/components/UseEffectHook.jsx
+++ 08-RC_lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -6,6 +6,8 @@
 //! componentDidMount,componentDidUpdate,ve componentWillUnmount
 //! metotlarinin bir birlesimi gibi dusunulebilir.
 
+import { useState, useEffect } from "react"
+
 //! useEffect(() => {
 //*   /* ComponentDidMount code */
 //! }, []);
@@ -27,3 +29,63 @@
 //*     //* componentWillUnmount code */
 //!   };
 //! }, [var1, var2]);
+
+
+
+const UseEffectHook = () => {
+const [count, setCount] = useState(0);
+
+//!ComponentDidMount
+// useEffect(() => {
+//  console.log("Mounted"); //! fetch, async-await, localStorage,setTimeout
+//  setTimeout(()=> {
+//     alert("Data Fetched")
+//  },3000) 
+
+// }, []);
+
+
+//!  componentDidMount + componentDidUpdate 
+// useEffect(() => {
+//  console.log("Mounted + Updated"); //! fetch, async-await, localStorage,setTimeout
+//  setTimeout(()=> {
+//     alert("Data Fetched")
+//  },1000) 
+
+// },[count]);
+
+
+
+//! componentDidMount +  componentWillUnmount
+const fetchData = () => {
+    console.log("data fetched");
+}
+
+
+useEffect(() => {
+    //! componentDidMount
+const timerId = setInterval(fetchData,1000);
+console.log("mounted");
+
+  return () => {
+    //!componentWillUnmount
+    clearInterval(timerId); //! hide basarsak duracak 
+    console.log("Unmounted");
+
+  }
+}, [])
+
+
+
+
+    console.log("rendered");
+  return (
+    <div className="container text-center mt-4">
+        <h1 className="text-danger">USEEFFECT</h1>
+        <h3>Count: {count}</h3>
+        <button className="btn btn-warning" onClick={() => setCount(count+1)}>INC</button>
+    </div>
+  )
+}
+
+export default UseEffectHook
