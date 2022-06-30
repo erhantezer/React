1533fa43faa6ca7aa503b8f0a309bc916d1108a6
Erhan <erhantezer68@gmail.com>
2022-06-30 21:26:30 +0300

    updated

--- 08-RC_lifecycle-useEffect/src/App.js
+++ 08-RC_lifecycle-useEffect/src/App.js
@@ -1,9 +1,18 @@
 import LifeCycleMethods from './components/LifeCycleMethods';
+import {useState} from 'react'
 
 function App() {
+  const [show, setShow] = useState(true)
+console.log(show);
   return (
-    <div className="container text-center mt-4">
-      <LifeCycleMethods />
+    <div className="container text-center mt-5">
+      <button className='btn btn-dark' onClick={()=>setShow(!show)}>
+       { show ? "Hide":"Show"}
+        </button>
+
+      {/* conditional rendering  (Short-circuit)*/}
+      {show && <LifeCycleMethods />}
+      
     </div>
   );
 }
