ecea00a12b8b92cf2f256153c2c134fba7d93d1f
Erhan <erhantezer68@gmail.com>
2022-07-05 23:30:07 +0300

    updated

--- 11-RC_styled-example/src/App.js
+++ 11-RC_styled-example/src/App.js
@@ -1,25 +1,23 @@
-import Header from "./components/Header";
-import { ThemeProvider } from "styled-components";
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStlyes } from './components/styles/Global';
 
 const theme = {
-  colors:{
-    header:'#fff',
+  colors: {
+    header: '#fff',
     body: '#fff',
-    footer: '#8a1c4a'
-
-  }
-}
-
-
+    footer: '#8A1C4A',
+  },
+  responsive: '768px',
+};
 const App = () => {
-  return(
-  
-  <ThemeProvider theme={theme}>
-    <Header/>
-  </ThemeProvider>
- 
-  ) 
-  
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStlyes />
+      <Header />
+    </ThemeProvider>
+  );
 };
 
 export default App;
+
