405a42a7b5553392eb79380d227fb84f811f62bc
Erhan <erhantezer68@gmail.com>
2022-07-13 20:57:33 +0300

    updated

---
+++ 14-15-RC_recipe-app-solution/src/router/AppRouter.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { GlobalStyles } from '../components/globalStyles/Global.styles';
+import Navbar from '../components/nav/Navbar';
+import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+import About from '../pages/about/About';
+
+
+const AppRouter = () => {
+  return (
+  <BrowserRouter>
+
+  <GlobalStyles/>
+  <Navbar/>
+
+    <Routes>
+       <Route path='/' element={<Home />}/>     
+       <Route path='login' element={<Login />}/>     
+       <Route path='about' element={<About/>} />     
+       <Route/>     
+       <Route/> 
+    </Routes>
+  
+  </BrowserRouter>
+  )
+};
+
+export default AppRouter;
