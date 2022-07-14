f09645eb111e2f1ee86c72586d30435d475a39ec
Erhan <erhantezer68@gmail.com>
2022-07-15 00:32:27 +0300

    updated

--- 14-15-RC_recipe-app-solution/src/pages/home/Home.jsx
+++ 14-15-RC_recipe-app-solution/src/pages/home/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
   const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
   const [recipes, setRecipes] = useState(null);
 
-  const APP_ID ="f6edb9f6";
-  const APP_KEY ="4ab957a75da653a00a88eb87864b8139";
+  const APP_ID =process.env.REACT_APP_APP_ID;
+  const APP_KEY =process.env.REACT_APP_APP_KEY;
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
