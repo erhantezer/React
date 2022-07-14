638dd5d4ba32572ca6eaee0ec163268617de0a81
Erhan <erhantezer68@gmail.com>
2022-07-14 21:07:30 +0300

    updated

--- 14-15-RC_recipe-app-solution/src/pages/home/Home.jsx
+++ 14-15-RC_recipe-app-solution/src/pages/home/Home.jsx
@@ -1,8 +1,34 @@
 import HomeStyle from './Home.style'
-
+import axios from 'axios';
+import Header from '../../components/header/Header'
+import { useEffect, useState } from 'react';
 const Home = () => {
+const APP_ID ="f6edb9f6";
+const APP_KEY ="4ab957a75da653a00a88eb87864b8139";
+
+const mealType=["Breakfast","Lunch","Dinner","Snack","Teatime"]
+const [query, setQuery] = useState("");
+const [selectedMeal, setSelectedMeal] = useState("");
+const [recipes, setRecipes] = useState("");
+
+
+const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+const getData = async() => {
+  try {
+    const {data} = await axios.get(url);
+  setRecipes(data.hits)
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
+
+
   return (
-    <div>Home</div>
+    <div>
+      <Header setQuery={setQuery} setSelectedMeal={setSelectedMeal} mealType={mealType} getData={getData}/>
+    </div>
   )
 }
 
