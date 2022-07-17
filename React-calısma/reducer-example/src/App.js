0b39d087ae773bb3bee08bb65a89468cfa537b44
Erhan <erhantezer68@gmail.com>
2022-07-17 14:36:25 +0300

    reducer

---
+++ React-calısma/reducer-example/src/App.js
@@ -0,0 +1,15 @@
+// import UseStateExample from './UseStateExample';
+
+import UserReducerExample from "./UseReducerExample";
+
+
+
+function App() {
+  return (
+    <div>
+      {/* <UseStateExample /> */}
+      <UserReducerExample/>
+    </div>
+  );
+}
+export default App;
