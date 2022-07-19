944608847e35381fffa2da0808f830102f72d556
Erhan <erhantezer68@gmail.com>
2022-07-19 03:17:01 +0300

    navbar

--- 17-RC_material-ui/material-intro/src/pages/Home.jsx
+++ 17-RC_material-ui/material-intro/src/pages/Home.jsx
@@ -1,11 +1,16 @@
+import Navbar from '../components/AppBarComp';
 import CardsGrid from '../components/CardsGrid';
 import MakeStylesComp from '../components/MakeStyleComp';
 import TextFieldComp from '../components/TextFieldComp';
 import TypoButtons from '../components/TypoButtons';
 
+
+
+
 const Home = () => {
   return (
     <>
+      <Navbar/>
       <TypoButtons /> <br /><br /><br /><br /><br />
       <CardsGrid /> <br /><br /><br /><br /><br />
       <TextFieldComp/>
