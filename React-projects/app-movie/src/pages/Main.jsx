eaf23c011a0bd21833332f7dac5fbe90497e59e2
Erhan <erhantezer68@gmail.com>
2022-07-21 01:45:52 +0300

    main1

--- React-projects/app-movie/src/pages/Main.jsx
+++ React-projects/app-movie/src/pages/Main.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState('');
 
-export default Main
+  
+  return (
+    <>
+    <div className="text-center d-flex justify-content-center align-items-center">
+      <form className="search">
+        <input
+          type="search"
+          className="search-input mt-5 me-3 p-3"
+          placeholder="Search a movie..."
+         onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit" className="btn btn-info mb-1">Search</button>
+      </form>
+    </div>
+
+    </>
+  );
+};
+
+export default Main;
