f051197419745b0cb628e12f0f43ab4b1f976699
Erhan <erhantezer68@gmail.com>
2022-07-21 15:36:55 +0300

    detail1

--- React-projects/app-movie/src/pages/MovieDetail.jsx
+++ React-projects/app-movie/src/pages/MovieDetail.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 const MovieDetail = () => {
+  const { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState('');
+
+
+
+
+
+  const API_KEY ="a9a90e58da935e5528540782b69aa0cf";
+  const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+  // const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
+  const baseImageUrl = 'https://image.tmdb.org/t/p/w1280';
+  const defaultImage =
+    'https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80';
+
   return (
     <div>MovieDetail</div>
   )
