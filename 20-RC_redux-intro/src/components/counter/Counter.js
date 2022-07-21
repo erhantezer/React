7eef64eebba2ea08552c870173d254aa2f306dc2
Erhan <erhantezer68@gmail.com>
2022-07-21 22:18:08 +0300

    redux

--- 20-RC_redux-intro/src/components/counter/Counter.js
+++ 20-RC_redux-intro/src/components/counter/Counter.js
@@ -1,14 +1,30 @@
 import './Counter.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {increment, reset, decrement} from '../../redux/actions/counterActioans.jsx'
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter)
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
-      <h1>counter:0</h1>
+      <h1>{counter}</h1>
       <div>
-        <button className="counter-button positive">increase</button>
-        <button className="counter-button zero">reset</button>
-        <button className="counter-button negative">decrease</button>
+        <button
+          className="counter-button positive"
+          // onClick={() => dispatch({ type: 'INCREMENT' })}
+          onClick={() => dispatch(increment())}
+        >
+          increase
+        </button>
+        <button className="counter-button zero"
+        // onClick={() => dispatch({ type: 'RESET' })}
+        onClick={() => dispatch(reset())}
+        >reset</button>
+        <button className="counter-button negative"    
+        // onClick={() => dispatch({ type: 'DECREMENT' })}
+        onClick={() => dispatch(decrement())}
+        >decrease</button>
       </div>
     </div>
   );
---
+++ 20-RC_redux-intro/src/redux/reducers/todoReducers.jsx
@@ -0,0 +1,13 @@
+const initialState = {}
+
+const todoReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+
+  case first:
+    return { ...state, ...payload }
+
+  default:
+    return state
+  }
+}
+export default todoReducer;
