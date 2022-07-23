604863cc5e8003280f198cb10738e619efc2933e
Erhan <erhantezer68@gmail.com>
2022-07-24 01:55:26 +0300

    updated

--- React-calısma/Redux-todo/src/components/todo/TodoList.jsx
+++ React-calısma/Redux-todo/src/components/todo/TodoList.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import TodoItem from './TodoItem'
 
 const TodoList = () => {
+
+
+    
   return (
     <div>
-        <TodoItem/>
+
+        <div>
+            {[]?.map((todo) => {
+                return (
+                    <TodoItem key={todo.id} {...todo}/>
+                )
+            })}
+        </div>
+
+        <div className='clear-wrapper'>
+            <button className='clear-button'>
+                Clear
+            </button>
+        </div>
+        
     </div>
   )
 }
