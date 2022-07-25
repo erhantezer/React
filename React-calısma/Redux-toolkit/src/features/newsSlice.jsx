6f37f4dbc8ff088760cb2ec9df9487f9fb8f5d38
Erhan <erhantezer68@gmail.com>
2022-07-25 14:25:42 +0300

    updated

--- React-calısma/Redux-toolkit/src/features/newsSlice.jsx
+++ React-calısma/Redux-toolkit/src/features/newsSlice.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios';
 
-const newsSlice = () => {
-  return (
-    <div>newsSlice</div>
-  )
+const initialState = {
+  newList: [],
+  loading: true
 }
+
+//? State'lerin API gibi async kaynaklardan gelen verilere gore guncellenmesi gerekebilir.
+//? Ancak boyle bir durumda async islem tamamlandiktan sonra state guncellenmelidir.
+//? Gonderilen api istegi ile dogrudan state guncellememelidir.
+//? Islemin tamamlanmasi ile gelen veriye gore state'in guncellenemsini saglamak
+//? adina bir arabirim kullanilmaktadir.
+//? Bu arabirim middleware denilir.Redux-Toolkit, default olarak Thunk kullanmaktadir.
+//! Thunk'ın amaci reducers'a islenmis sonuclari gondermeden once gecikmeli asenkron ismlerinin yurutulmesini saglamaktir.
+
+const API_KEY ="44e23dc79f324a2380981ce37f159336";
+
+export const getNews = createAsyncThunk(
+  'news/getNews', //!actioan type ismi
+  //! async callback fun
+  async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
+    try {
+      const {data} = await axios.get(url);
+      return data.articles;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+)
+
+const newsSlice = createSlice({
+  name:"news",
+  initialState,
+  educers: {
+    clearNewsList: (state) => {
+      state.newsList = [];
+    },
+  },
+  extraReducers: {
+    [getNews.pending]: (state) => {
+      state.loading = true;
+    },
+    [getNews.fulfilled]: (state, {payload}) => {
+      state.loading=false;
+      state.newList = payload;
+    },
+    [getNews.rejected]: (state) => {
+      state.loading = false;
+    },
+  },
+});
+  //! baska slice'lardaki tanimlanan action'lara cevap vermek
+//! bilhassa createAsyncThunk tarafindan olusturulan action'lara
+//! cevap vermek icin kullanilir.
+
+export const { clearNewsList } = newsSlice.actions;
+
 
 export default newsSlice
--- React-calısma/Redux-toolkit/src/router/AppRouter.jsx
+++ React-calısma/Redux-toolkit/src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import PrivateRouter from "./PrivateRouter";
 import Login from "../pages/Login";
 import Navbar from '../components/Navbar';
+import News from '../pages/News';
 
 const AppRouter = () => {
   return (
@@ -9,7 +10,7 @@ const AppRouter = () => {
     <Navbar/>
     <Routes>
       <Route path='/' element={<PrivateRouter/>}>
-        <Route path='' element={<Login/>}/>
+        <Route path='' element={<News/>}/>
       </Route>
       <Route path='login' element={<Login/>} />
     </Routes>
