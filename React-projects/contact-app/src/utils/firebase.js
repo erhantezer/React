8a4b7052b1777593d38678cc62fb8a35ceea18e2
Erhan <erhantezer68@gmail.com>
2022-07-30 00:11:49 +0300

    updated

--- React-projects/contact-app/src/utils/firebase.js
+++ React-projects/contact-app/src/utils/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyCl3n9q_PwoAa5MQBK-R-LJr_7eYofmEtc",
     authDomain: "contact-db-50063.firebaseapp.com",
@@ -11,6 +12,7 @@ const firebaseConfig = {
     appId: "1:304778150564:web:4f0ca0ddec265892051c31",
     measurementId: "G-JM2461MPYM"
   };
+
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
