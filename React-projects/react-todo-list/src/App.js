c1f4afd3ff8fe635a07dc96d54854a4550b57ad5
Erhan <erhantezer68@gmail.com>
2022-07-31 00:38:40 +0300

    updated

--- React-projects/react-todo-list/src/App.js
+++ React-projects/react-todo-list/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>APP</h1>
+      
     </div>
   );
 }
--- React-projects/react-todo-list/src/index.js
+++ React-projects/react-todo-list/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer} from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <>
+    <App />
+    <ToastContainer/>
+    </>
+);
