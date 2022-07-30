733e568de5908e21d28a075528d732e9c8f6921d
Erhan <erhantezer68@gmail.com>
2022-07-30 20:46:30 +0300

    updated

--- React-projects/contact-app/src/utils/firebase.js
+++ React-projects/contact-app/src/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+// import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
@@ -15,5 +15,6 @@ const firebaseConfig = {
 
   
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
+  const firebase= initializeApp(firebaseConfig);
+  // const database = getDatabase(app);
+  export default firebase;
